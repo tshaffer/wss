c703f07bc97c8d04b64d871ca9a73fa7092072a2
Ted Shaffer <ted@brightsign.biz>
2018-11-18 04:44:40 -0800

    start server and open wss

--- wss.js
+++ wss.js
@@ -1,11 +1,23 @@
-const WebSocket = require('ws');
- 
-const wss = new WebSocket.Server({ port: 8080 });
- 
+var WebSocket = require('ws');
+var express = require('express');
+var http = require('http');
+
+var app = express()
+
+var server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
 wss.on('connection', function connection(ws) {
+  console.log('connection event received');
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
  
   ws.send('something');
 });
+
+server.listen(process.env.PORT || 8999, () => {
+  console.log(`Server started on port ${server.address().port}`);
+});
+
